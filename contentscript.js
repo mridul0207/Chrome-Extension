273a324a3dde6ff27720db9d4af38eac0bae226f
mridul0702 <guptamridul0207@gmail.com>
2023-09-22 00:23:47 +0530

    Final Commit- Task Completed

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var myButton = document.getElementById('my-button');
-  myButton.addEventListener('click', function() {
-    alert('Hello, world!');
-  });
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    // If the message is a request for the title of the current tab, send it back to the popup script.
+    if (message === 'getTabTitle') {
+      chrome.tabs.query({active: true}, function(tabs) {
+        sendResponse(tabs[0].title);
+      });
+    }
 });
---
+++ popup.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    var myButton = document.getElementById('my-button');
+    var tabTitle = document.getElementById('tab-title');
+
+    myButton.addEventListener('click',()=>{
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            console.log(tabs[0].url);
+            console.log(tabs[0].title);
+            tabTitle.innerHTML = tabs[0].title;
+        });
+    })
+})
