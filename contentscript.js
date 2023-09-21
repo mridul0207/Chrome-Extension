15c85556a532405ee6e9ab156d87c49f00a14722
mridul0702 <guptamridul0207@gmail.com>
2023-09-21 23:52:31 +0530

    Created a sample extension to test

---
+++ contentscript.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var myButton = document.getElementById('my-button');
+  myButton.addEventListener('click', function() {
+    alert('Hello, world!');
+  });
+});
